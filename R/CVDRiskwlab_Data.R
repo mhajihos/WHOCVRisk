1ffceec5d29c4968ee6d2349f962f3ba9961a621
mhajihos <50219979+mhajihos@users.noreply.github.com>
2021-12-03 12:46:43 -0700

    Add files via upload

---
+++ R/CVDRiskwlab_Data.R
@@ -0,0 +1,26 @@
+#' WHOCVRisk with Lab Data
+#'
+#' A dataset containing the Region, Age, Diabetes, Gender, Smoke, Cholesterol, SBP, and Risk.
+#'  The variables are as follows:
+#'
+#' \itemize{
+#'   \item Region. Region of residence including Andean Latin America, Australasia, Caribbean, Central Asia, Central Europe,
+#'   Central Latin America, Central Sub-Saharan Africa, East Asia, Eastern Europe, Eastern Sub-Saharan Africa,
+#'   High-Income Asia Pacific, High-Income North America, North Afrika and Middle East, Oceania, South Asia,
+#'   Southeast Asia, Southern Latin America, Southern Sub-Saharan Africa, Tropical Latin America,
+#'   Western Europe, Western Sub-Saharan Africa.
+#'   \item DM. Diabetes including "Yes" Diabetic and "No" Non-Diabetic.
+#'   \item Gender. Gender including "M" Male and "F" Female.
+#'   \item Smoke. Smoke including "Smoker" and "Non-smoker"
+#'   \item Age. Age including age groups: 40-44, 45-49, 50-54, 55-59, 60-64, 65-69, 70-74.
+#'   \item Cholesterol. Total cholesterol in (mmol/l) in  including <20, 20-24, 25-29, 30-35, >=35.
+#'   \item SBP. SBP in (mmHg) including <4, 4-4.9, 5-5.9, 6-6.9, >=7.
+#'   \item Risk. orld Health Organization cardiovascular disease risk revised estimates based on 21 global regions.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name CVDRisk
+#' @usage data(CVDRiskwlab)
+#' @format A data frame
+#' @export
